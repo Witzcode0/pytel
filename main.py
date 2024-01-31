c5bf39f8b6ab5f4f8e0728af7a450a1b131fddce
Witzcode0 <brijesh.gondaliya07@gmail.com>
2024-01-31 20:47:53 +0530

    set datetime

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from dotenv import load_dotenv
 import telebot
 import os
 import string
+import json
 
 import constants
 from db_mysql import fatch_countries
 from get_api_data import get_country_data_from_api
+import datetime
+import pytz
 
 
 load_dotenv()
@@ -68,14 +71,45 @@ def  get_first_letter_of_country(message):
     bot.send_message(user_chat_id, "Choose the country.", reply_markup=countries_markup)
     bot.register_next_step_handler(message, get_specifi_country)
 
+def get_current_time_on_timezone(timezone_str):
+    timezone = pytz.timezone(timezone_str)
+
+    # Get the current date and time in the specified timezone
+    current_datetime = datetime.datetime.now(timezone)
+
+    # Format the current date and time in AM/PM format
+    formatted_datetime = current_datetime.strftime("%d-%m-%Y %I:%M %p")
+    return formatted_datetime
+
+
+def get_country_by_name(json_file, country_name):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+
+    matching_countries = [country for country in data if country["name"].lower() == country_name.lower()]
+
+    country_timezone = ''
+    if matching_countries and len(matching_countries) != 0:
+        for country in matching_countries:
+            country_timezone += country['timezone'] + ':' + f'{get_current_time_on_timezone(country['timezone'])}'+ ',\n'
+    if len(country_timezone) == 0:
+        result = "No Data Found"
+    else:
+        result = country_timezone
+
+    return result
+
 def get_specifi_country(message):
     user_chat_id = message.chat.id
     country = fatch_countries.get_specific_country_details(message.text)
+    
     # {get_country_data_from_api(country[1])}
+
     country_data = f"""
     Name : {country[1]} \nFlag : {country[3]}\nIndependent : {country[4]}\nOfficial_name : {country[6]}\nCapital City : {country[7]}\nContinent : {country[8]}\nMembers of : {country[9]}\nTotal Area : {country[11]}\nCurrency : {country[15].split("(")[0].strip()}\nCalling Code: {country[16]}\n Internet Tld : {country[17]}
     """
     bot.send_message(user_chat_id,country_data)
+    bot.send_message(user_chat_id, f"Timezone:Current_Date&Time : {get_country_by_name("country_timezone.json", country[1])}")
     bot.send_message(user_chat_id, "Start Again /start")
 
 
